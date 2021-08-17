46226738e27fec018c8d73223c1dbdadf90b6047
Angel Martinez <angel.martineztrc@gmail.com>
2021-08-17 16:11:36 -0500

    chore(src):  install swiperjs and add some markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-function App() {
+import { Slider } from './components/Slider';
+
+export const App = () => {
   return (
-    <div>
-      <p>Hello, World!</p>
+    <div className="bg-gray-900">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="container mx-auto">
+          <h1 className="mb-12 text-center text-white text-4xl font-medium">
+            Testimonials
+          </h1>
+          <Slider />
+        </div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/Slider.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Slider = ({}: SliderProps) => {
+  return <div></div>;
+};
+
+type SliderProps = {};
