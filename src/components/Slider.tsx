89407286539aaeaec5f4e568b0954406ef197444
Angel Martinez <angel.martineztrc@gmail.com>
2021-08-17 16:21:35 -0500

    feat(Slider):  create an slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export const Slider = ({}: SliderProps) => {
-  return <div></div>;
+  return (
+    <Swiper className="w-full" slidesPerView={2} spaceBetween={48}>
+      <SwiperSlide>
+        <p className="text-gray-400">Slide 1</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <p className="text-gray-400">Slide 2</p>
+      </SwiperSlide>
+    </Swiper>
+  );
 };
 
 type SliderProps = {};
