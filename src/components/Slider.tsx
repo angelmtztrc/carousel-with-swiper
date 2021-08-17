d96871b8a5012163b28a6c509e1f3687a99b8376
Angel Martinez <angel.martineztrc@gmail.com>
2021-08-17 17:11:06 -0500

    feat(Slider):  add autoplay and loop functionality

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SwiperCore, { Autoplay, A11y } from 'swiper/core';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // components
@@ -6,11 +7,19 @@ import { SlideContent } from './SlideContent';
 // interfaces
 import { Person } from '../interfaces/person.interface';
 
+SwiperCore.use([Autoplay, A11y]);
+
 export const Slider = ({ people }: SliderProps) => {
   return (
-    <Swiper className="w-full" slidesPerView={2} spaceBetween={48}>
+    <Swiper
+      className="w-full"
+      loop={true}
+      slidesPerView={2}
+      spaceBetween={48}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+    >
       {people.map(person => (
-        <SwiperSlide key={person.id}>
+        <SwiperSlide key={person.id} className="h-full">
           <SlideContent person={person} />
         </SwiperSlide>
       ))}
