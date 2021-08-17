a95268ae2a5056ab9f2163bad4e2e3d9931f82f7
Angel Martinez <angel.martineztrc@gmail.com>
2021-08-17 16:56:04 -0500

    feat(Slider):  display information from a json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+// api
+import data from './api/data.json';
+// components
 import { Slider } from './components/Slider';
+// interfaces
+import { Person } from './interfaces/person.interface';
 
 export const App = () => {
+  const people: Person[] = data;
+
   return (
     <div className="bg-gray-900">
       <div className="flex items-center justify-center min-h-screen">
@@ -10,7 +17,7 @@ export const App = () => {
           <h1 className="mb-12 text-center text-white text-4xl font-medium">
             Testimonials
           </h1>
-          <Slider />
+          <Slider people={people} />
         </div>
       </div>
     </div>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+// components
 import { SlideContent } from './SlideContent';
+// interfaces
+import { Person } from '../interfaces/person.interface';
 
-export const Slider = ({}: SliderProps) => {
+export const Slider = ({ people }: SliderProps) => {
   return (
     <Swiper className="w-full" slidesPerView={2} spaceBetween={48}>
-      <SwiperSlide>
-        <SlideContent />
-      </SwiperSlide>
-      <SwiperSlide>
-        <SlideContent />
-      </SwiperSlide>
-      <SwiperSlide>
-        <SlideContent />
-      </SwiperSlide>
+      {people.map(person => (
+        <SwiperSlide key={person.id}>
+          <SlideContent person={person} />
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
 
-type SliderProps = {};
+type SliderProps = {
+  people: Person[];
+};
+
+const defaultProps = {
+  people: []
+};
+
+Slider.defaultProps = defaultProps;
