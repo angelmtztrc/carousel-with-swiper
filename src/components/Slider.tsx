c603be452f35ecc9cc1ad0c505d46bfa6dc96278
Angel Martinez <angel.martineztrc@gmail.com>
2021-08-17 16:38:06 -0500

    feat(SlideContent):  display some content on each slide

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import { SlideContent } from './SlideContent';
+
 export const Slider = ({}: SliderProps) => {
   return (
     <Swiper className="w-full" slidesPerView={2} spaceBetween={48}>
       <SwiperSlide>
-        <p className="text-gray-400">Slide 1</p>
+        <SlideContent />
       </SwiperSlide>
       <SwiperSlide>
-        <p className="text-gray-400">Slide 2</p>
+        <SlideContent />
+      </SwiperSlide>
+      <SwiperSlide>
+        <SlideContent />
       </SwiperSlide>
     </Swiper>
   );
